6e87f440360fc2ee3cae0e6f038ad35dd9c5b800
byDX8lIZVX65 <jaykdoe@gmail.com>
2024-10-11 10:07:42 -0700

    /

--- actions/getHourlyData.ts
+++ actions/getHourlyData.ts
@@ -6,7 +6,8 @@ export const getHourlyData = async ({
   lon: string
 }) => {
   const data = await fetch(
-    `https://${process.env.VERCEL_URL}/api/weather/hourly?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
+    // `https://${process.env.VERCEL_URL}/api/weather/hourly?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
+    `https://${process.env.VERCEL_URL}/api/weather/daily_forecast?lat=${process.env.lat}&lon=${process.env.lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
   )
   if (!data.ok) {
     throw new Error("Failed to fetch data")
