4704cb8d9552b0aa48fcc783e1c3784319fdb6f3
byDX8lIZVX65 <jaykdoe@gmail.com>
2024-10-13 10:24:43 -0700

    /

--- actions/getHourlyData.ts
+++ actions/getHourlyData.ts
@@ -7,7 +7,7 @@ export const getHourlyData = async ({
 }) => {
   const data = await fetch(
     // `https://${process.env.VERCEL_URL}/api/weather/hourly?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
-    `http://localhost:3001/api/weather/daily_forecast?lat=${process.env.lat}&lon=${process.env.lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
+    `https://${process.env.VERCEL_URL}/api/weather/daily_forecast?lat=${process.env.lat}&lon=${process.env.lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
   )
   if (!data.ok) {
     throw new Error("Failed to fetch data")
--- actions/getTenDayForecast.ts
+++ actions/getTenDayForecast.ts
@@ -7,7 +7,7 @@ export const getTenDayForecast = async ({
 }) => {
   const data = await fetch(
     // `https://${process.env.VERCEL_URL}/api/weather/daily_forecast?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
-    `http://localhost:3001/api/weather/daily_forecast?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
+    `https://${process.env.VERCEL_URL}/api/weather/daily_forecast?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
   )
   if (!data.ok) {
     throw new Error("Failed to fetch data")
